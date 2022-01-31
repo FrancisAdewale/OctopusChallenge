52911314d1579e6311f128fbcdf0956e38816b87
Francis Adewale <francisadewale1992@gmail.com>
2022-01-31 18:25:50 +0000

    add espresso test for lazy column/ recycler view

---
+++ README.md
@@ -0,0 +1,26 @@
+# OctopusChallenge
+
+Technologies:
+Jetpack Compose
+Espresso - UI Testing
+Retrofit - Api Calls
+
+
+I used MVVM Archiecture pattern as it's the one I enjoy using the most.
+
+I used retofit to handle any network api calls as it's the library I have the most comfort using. I had 3 end points:
+
+One to list all breeds provided by backend called "getAllBreeds"
+One to list breed info based on clicked breed item on the main activity/interface called "getBreedInfo"
+One to display all the related images based on the breed id provided by the main activity called "getBreedImage" this should be plural
+
+I used espresso to test the lazy column aka recycler view on the main activity, but i did not take a TDD approach. I Wrote the main code
+and tested the UI after due to time. 
+
+Regarding UI/UX I am not too strong on this point as I usually rely on the design team for this. I also wasted a bit of time on the second end point due to a misunderstanding
+of the json response, it was a simple error that I spent too much time on.
+
+I was thinking about using Koin for dependency injection but I didn't get around to it.
+
+
+
--- app/src/androidTest/java/com/example/octopuschallenge/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/octopuschallenge/ExampleInstrumentedTest.kt
@@ -1,12 +1,18 @@
 package com.example.octopuschallenge
 
-import androidx.test.platform.app.InstrumentationRegistry
+import androidx.compose.ui.test.assertCountEquals
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onChildren
+import androidx.compose.ui.test.onNodeWithTag
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.octopuschallenge.ui.theme.OctopusChallengeTheme
 
 import org.junit.Test
 import org.junit.runner.RunWith
 
-import org.junit.Assert.*
+import org.junit.Rule
+
+
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -15,10 +21,24 @@ import org.junit.Assert.*
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+
+    val composeTestRule = createAndroidComposeRule<MainActivity>()
+        @Rule get
+
     @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.octopuschallenge", appContext.packageName)
+    fun testBreedListScreenIfBreedsListIsEmpty() {
+
+        composeTestRule.setContent {
+            OctopusChallengeTheme {
+                GetBreedist(emptyList())
+            }
+        }
+
+        composeTestRule.onNodeWithTag(BREED_LIST_TEST_TAG)
+            .onChildren()
+            .assertCountEquals(0)
+
     }
+
+
 }
